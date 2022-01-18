304b5a2b225e84b1ce09abc6109d45a0b862009c
Annie Huang <annie.hn@gmail.com>
2022-01-18 17:51:07 +1100

    Pass a payload to the action

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './styles.css';
 import { useDispatch, useSelector } from 'react-redux';
-import { decrement, increment } from './redux/counter';
+import { decrement, increment, incrementByAmount } from './redux/counter';
 
 function App() {
   const { count } = useSelector((state) => state.counter);
@@ -11,6 +11,9 @@ function App() {
       <h1> The count is: {count}</h1>
       <button onClick={() => dispatch(increment())}>increment</button>
       <button onClick={() => dispatch(decrement())}>decrement</button>
+      <button onClick={() => dispatch(incrementByAmount(33))}>
+        Increment by 33
+      </button>
     </div>
   );
 }
