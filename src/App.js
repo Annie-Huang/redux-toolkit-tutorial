e9bbd721dbf6f9446a5cf5d2ccb6497047d51082
Annie Huang <annie.hn@gmail.com>
2022-01-18 17:47:49 +1100

    Get the count state from userSelector and use useDspatch to dispitch an action

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import { useState } from 'react';
 import './styles.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { decrement, increment } from './redux/counter';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const { count } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
 
   return (
     <div className='App'>
       <h1> The count is: {count}</h1>
-      <button onClick={() => setCount(count + 1)}>increment</button>
-      <button onClick={() => setCount(count - 1)}>decrement</button>
+      <button onClick={() => dispatch(increment())}>increment</button>
+      <button onClick={() => dispatch(decrement())}>decrement</button>
     </div>
   );
 }
